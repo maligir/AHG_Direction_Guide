7fbd022267df07e21b6f2fa943e06b42786748e7
Rahul Maligi <maligir@utexas.edu>
2022-05-03 20:01:31 -0500

    Added camera access to facial-verification

--- facial_recognition/facial-verification.py
+++ facial_recognition/facial-verification.py
@@ -1,3 +1,4 @@
+from pickle import FALSE
 from matplotlib import pyplot
 from PIL import Image
 from numpy import asarray
@@ -5,60 +6,108 @@ from scipy.spatial.distance import cosine
 from mtcnn.mtcnn import MTCNN
 from keras_vggface.vggface import VGGFace
 from keras_vggface.utils import preprocess_input
+import cv2
+import os, shutil
+
+class FaceVerification:
+
+	def __init__(self, default_image='default.jpg'):
+		self.detector = MTCNN()
+		self.model = VGGFace(model='resnet50', include_top=False, input_shape=(224, 224, 3), pooling='avg')
+		self.defaultFaces = self.extract_face(default_image)
  
-# extract a single face from a given photograph
-def extract_face(filename, required_size=(224, 224)):
-	# load image from file
-	pixels = pyplot.imread(filename)
-	# create the detector, using default weights
-	detector = MTCNN()
-	# detect faces in the image
-	results = detector.detect_faces(pixels)
-	# extract the bounding box from the first face
-	x1, y1, width, height = results[0]['box']
-	x2, y2 = x1 + width, y1 + height
-	# extract the face
-	face = pixels[y1:y2, x1:x2]
-	# resize pixels to the model size
-	image = Image.fromarray(face)
-	image = image.resize(required_size)
-	face_array = asarray(image)
-	return face_array
- 
-# extract faces and calculate face embeddings for a list of photo files
-def get_embeddings(filenames):
-	# extract faces
-	faces = [extract_face(f) for f in filenames]
-	# convert into an array of samples
-	samples = asarray(faces, 'float32')
-	# prepare the face for the model, e.g. center pixels
-	samples = preprocess_input(samples, version=2)
-	# create a vggface model
-	model = VGGFace(model='resnet50', include_top=False, input_shape=(224, 224, 3), pooling='avg')
-	# perform prediction
-	yhat = model.predict(samples)
-	return yhat
- 
-# determine if a candidate face is a match for a known face
-def is_match(known_embedding, candidate_embedding, thresh=0.4):
-	# calculate distance between embeddings
-	score = cosine(known_embedding, candidate_embedding)
-	if score <= thresh:
-		print('>face is a Match (%.3f <= %.3f)' % (score, thresh))
-	else:
-		print('>face is NOT a Match (%.3f > %.3f)' % (score, thresh))
- 
-# define filenames
-filenames = ['rahul_1.jpeg', 'rahul_2.jpeg',
-	'rahul_3.jpeg', 'vrishank_1.jpeg', 'siri_1.jpeg', 'rohit_1.jpeg']
-# get embeddings file filenames
-embeddings = get_embeddings(filenames)
-# define sharon stone
-sharon_id = embeddings[0]
-# verify known photos of sharon
-print('Positive Tests')
-is_match(embeddings[0], embeddings[1])
-is_match(embeddings[0], embeddings[2])
-# verify known photos of other people
-print('Negative Tests')
-is_match(embeddings[0], embeddings[3])
+	# extract a single face from a given photograph
+	def extract_face(self, filename, required_size=(224, 224)):
+		# load image from file
+		pixels = pyplot.imread(filename)
+		# create the detector, using default weights
+		detector = MTCNN()
+		# detect faces in the image
+		results = detector.detect_faces(pixels)
+		# extract the bounding box from the first face
+		if len(results) == 0:
+			return self.defaultFaces
+		x1, y1, width, height = results[0]['box']
+		x2, y2 = x1 + width, y1 + height
+		# extract the face
+		face = pixels[y1:y2, x1:x2]
+		# resize pixels to the model size
+		image = Image.fromarray(face)
+		image = image.resize(required_size)
+		face_array = asarray(image)
+		return face_array
+	
+	# extract faces and calculate face embeddings for a list of photo files
+	def get_embeddings(self, filenames):
+		# extract faces
+
+		faces = [self.extract_face(f) for f in filenames]
+		# convert into an array of samples
+		samples = asarray(faces, 'float32')
+		# prepare the face for the model, e.g. center pixels
+		samples = preprocess_input(samples, version=2)
+		# perform prediction
+		yhat = self.model.predict(samples)
+		return yhat
+	
+	# determine if a candidate face is a match for a known face
+	def is_match(self, known_embedding, candidate_embedding, thresh=0.4):
+		# calculate distance between embeddings
+		score = cosine(known_embedding, candidate_embedding)
+		if score <= thresh:
+			print('>face is a Match (%.3f <= %.3f)' % (score, thresh))
+		else:
+			print('>face is NOT a Match (%.3f > %.3f)' % (score, thresh))
+
+	def clean_folder(self, folder):
+		for filename in os.listdir(folder):
+			file_path = os.path.join(folder, filename)
+			try:
+				if os.path.isfile(file_path) or os.path.islink(file_path):
+					os.unlink(file_path)
+				elif os.path.isdir(file_path):
+					shutil.rmtree(file_path)
+			except Exception as e:
+				print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+	def run(self):
+		# Create window connected to camera
+		cv2.namedWindow("Facial Verification")
+		vc = cv2.VideoCapture(0)
+		# Try to read in first frame
+		if vc.isOpened():
+			rval, frame = vc.read()
+		else:
+			rval = False
+		#Create Constants
+		filenames = []
+		frameCounter = 0
+		fileCounter = 0
+		os.chdir("./img")
+		# infinite loop until esc key is pressed
+		while rval:
+			# Get frame from camera and wait a few milliseconds
+			cv2.imshow("preview", frame)
+			rval, frame = vc.read()
+			key = cv2.waitKey(20)
+			if key == 27 or not rval: # exit on ESC
+				break
+			# Every 120 frames, save a frame and run facial recognition
+			if(frameCounter%120 == 0):
+				filename = "img" + str(fileCounter) + ".jpg"
+				cv2.imwrite(filename, frame)
+				filenames.append(filename)
+				embeddings = self.get_embeddings(filenames)
+				self.is_match(embeddings[0], embeddings[fileCounter])
+				fileCounter += 1
+			#update frame counter
+			frameCounter+=1
+		# Clean up and Destroy all windows and files
+		cv2.destroyWindow("preview")
+		vc.release()
+		self.clean_folder(".")
+
+
+app = FaceVerification()
+app.run()
+
